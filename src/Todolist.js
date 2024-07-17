acf3fd4bbc553e2d90920d7ff189747c87b6496d
NaqiyyahMurshidah <naqiyyahmurshidah1@gmail.com>
2024-07-17 23:35:22 +0800

    add local Storage

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,21 +1,35 @@
 //parent component
 //for each task in the list, this component will render a TodoItem component
-import { useState } from "react"; //use state is a hook that allows you to have state variables in functional components
+import { useState, useEffect } from "react"; //use state is a hook that allows you to have state variables in functional components
 import TodoItem from "./TodoItem"; //this is to import the TodoItem component
 
 function Todolist() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctor Appointment",
-      completed: true,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      completed: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState(() => {
+    const storageTasks = JSON.parse(localStorage.getItem("tasks"));  //fetch the tasks from local storage
+    return storageTasks ? storageTasks : [
+      {
+        id: 1,
+        text: "Learn React",
+        completed: false
+      },
+      {
+        id: 2,
+        text: "Learn Firebase",
+        completed: false
+      },
+      {
+        id: 3,
+        text: "Learn GraphQL",
+        completed: false
+      },
+    ]
+  })
+  
+  //add object to local storage whenever tasks change
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks])
+  
 
   const [text, setText] = useState("");
   function addTask(text) { //adding task 
